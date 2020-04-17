92d25e9a00adde3dad01912e564857b8afacd72e
Pavel <megamozg19991999@mail.ru>
2020-04-18 02:28:04 +0300

    Добавлено архивирование

--- archivator.cpp
+++ archivator.cpp
@@ -2,7 +2,11 @@
 #include <cstring>
 #include <vector>
 #include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
 using namespace std;
 
 
@@ -53,6 +57,47 @@ vector<string> find_files(string current_dir)
     return files;
 }
 
+
+void pack(string dir,vector<string> files)
+{
+    struct stat statbuf;
+    char mode[6], buff[1024];
+    int out1, out2;
+    string path1("./"+dir+"_arch.txt");
+    out1 = open(path1.c_str(), O_WRONLY|O_CREAT, S_IREAD|S_IWRITE);
+    string path2("./"+dir+"_data.txt");
+    out2 = open(path2.c_str(), O_WRONLY|O_CREAT, S_IREAD|S_IWRITE);
+    if (out1==-1)
+    {
+        close(out1);
+        unlink(path1.c_str());
+        out1 = open(path1.c_str(), O_CREAT, S_IREAD|S_IWRITE);
+    }
+    if (out2==-1)
+    {
+        close(out2);
+        unlink(path2.c_str());
+        out2 = open(path2.c_str(), O_CREAT, S_IREAD|S_IWRITE);
+    }
+    for (unsigned int i=0; i<files.size();i++)
+    {
+        lstat(files[i].c_str(), &statbuf);
+        path1 = files[i] + "\n";
+        write(out1, path1.c_str(),path1.size());
+        sprintf(mode,"%i\n",statbuf.st_mode);
+        write(out1, mode, 6);
+        int in = open(files[i].c_str(), O_RDONLY, S_IREAD);
+        while (read(in,buff,sizeof(buff)))
+        {
+            write(out2,buff,sizeof(buff));
+        }
+        write(out2,"stop_slovo\n",11);
+    }
+    close(out1);
+    close(out2);
+}
+
+
 int main(int n, char **argc)
 {
 	if (n > 1)
@@ -60,8 +105,14 @@ int main(int n, char **argc)
         string mode(argc[1]);
         string dir(argc[2]);
         vector<string> files(find_files(dir));
-        for(int i=0; i<files.size();i++)
-            cout<<files[i]<<endl;
+        if (mode == "pack")
+        {
+            pack(dir,files);
+        }
+        if (mode == "unpack")
+        {
+            //unpack();
+        }
     }
     else 
     {
